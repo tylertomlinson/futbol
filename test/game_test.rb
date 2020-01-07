3726bc581b3cb07719221bc6136fd1111539ed0f
David Atkins <atki1080@gmail.com>
2020-01-07 13:04:51 -0700

    Update game_test

--- test/game_test.rb
+++ test/game_test.rb
@@ -3,7 +3,13 @@ require './lib/game'
 
 class GameTest < Minitest::Test
   def setup
-    @game = Game.new({season: "20122013", away_goals: 3, home_goals: 4})
+    @game = Game.new({
+      season: "20162017",
+      away_goals: 2,
+      home_goals: 3,
+      home_team_id: 24,
+      away_team_id: 20,
+      game_id: 2016030171})
   end
 
   def test_it_exists
@@ -11,16 +17,24 @@ class GameTest < Minitest::Test
   end
 
   def test_it_has_attributes
-    assert_equal "20122013", @game.season
-    assert_equal 3, @game.away_goals
-    assert_equal 4, @game.home_goals
+    assert_equal "20162017", @game.season
+    assert_equal 2, @game.away_goals
+    assert_equal 3, @game.home_goals
+    assert_equal 24, @game.home_team_id
+    assert_equal 20, @game.away_team_id
+    assert_equal 2016030171, @game.game_id
   end
 
   def test_can_get_total_score
-    assert_equal 7, @game.total_score
+    assert_equal 5, @game.total_score
   end
 
   def test_can_get_difference_between_scores
     assert_equal 1, @game.difference_between_score
+  end
+
+  def test_it_can_find_opponenent_id
+    assert_equal 24, @game.opponent_id(20)
+    assert_equal 20, @game.opponent_id(24)
   end
 end
