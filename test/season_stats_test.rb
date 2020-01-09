ad69a5eff32a89ac0580bd455c2de92a397c16cf
David Atkins <atki1080@gmail.com>
2020-01-08 17:41:56 -0700

    Add seasonal_summary test case

--- test/season_stats_test.rb
+++ test/season_stats_test.rb
@@ -1,18 +1,32 @@
 require './test/test_helper'
 require './lib/game_collection'
 require './lib/game_teams_collection'
+require './lib/game_stats'
 require './lib/season_stats'
 
 class SeasonStatsTest < Minitest::Test
   def setup
+    # @game_collection = GameCollection.new("./test/fixtures/games_truncated.csv")
+    # @game_teams_collection = GameTeamsCollection.new("./test/fixtures/game_teams_truncated.csv")
+    # @season_stats = SeasonStats.new(@game_collection, @game_teams_collection)
+    #
+    # @total_game_collection = GameCollection.new("./data/games.csv")
+    # @total_game_teams_collection = GameTeamsCollection.new("./data/game_teams.csv")
+    #
+    # @total_season_stats = SeasonStats.new(@total_game_collection, @total_game_teams_collection)
     @game_collection = GameCollection.new("./test/fixtures/games_truncated.csv")
     @game_teams_collection = GameTeamsCollection.new("./test/fixtures/game_teams_truncated.csv")
-    @season_stats = SeasonStats.new(@game_collection, @game_teams_collection)
+
+    @game_stats = GameStats.new(@game_collection)
+
+    @season_stats = SeasonStats.new(@game_stats, @game_teams_collection)
 
     @total_game_collection = GameCollection.new("./data/games.csv")
     @total_game_teams_collection = GameTeamsCollection.new("./data/game_teams.csv")
 
-    @total_season_stats = SeasonStats.new(@total_game_collection, @total_game_teams_collection)
+    @total_game_stats = GameStats.new(@total_game_collection)
+
+    @total_season_stats = SeasonStats.new(@total_game_stats, @total_game_teams_collection)
   end
 
   def test_it_exists
@@ -33,7 +47,12 @@ class SeasonStatsTest < Minitest::Test
   end
 
   def test_it_can_make_seasonal_summary
-    @total_season_stats.seasonal_summary(16)
+    summary = @total_season_stats.seasonal_summary(18)["20152016"][:postseason]
+    assert_equal 0.36, summary[:win_percentage]
+    assert_equal 25, summary[:total_goals_scored]
+    assert_equal 33, summary[:total_goals_against]
+    assert_equal 1.79, summary[:average_goals_scored]
+    assert_equal 2.36, summary[:average_goals_against]
   end
 
   # def test_it_can_make_season_game_teams_array
